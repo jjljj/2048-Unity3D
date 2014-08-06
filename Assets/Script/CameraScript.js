7c1470d57803e9af948b3c3953ecaf7040023187
ljj <ljjcode@gmail.com>
2014-08-06 23:44:20 +0800

    为每个数字配上一个食物的预设，可以用键盘控制镜头： j：相机左移 l：相机右移 i：相机上移 k：相机下移 o：相机前移 p：相机后移 n：相机向左旋转 m：相机向右旋转

---
+++ Assets/Script/CameraScript.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var speed : float = 1.0;
+var rotationSpeed : float = 10.0;
+
+
+function Start () {
+
+}
+
+function Update () {
+	var left_right : float = Input.GetAxis("CCV")*speed;
+	var up_down : float = Input.GetAxis("CCH")*speed;
+	var fore_back : float = Input.GetAxis("CCF")*speed;
+	var rotation : float = Input.GetAxis("Jump")*rotationSpeed;
+	
+	left_right *= Time.deltaTime;
+	up_down *= Time.deltaTime;
+	fore_back *= Time.deltaTime;
+	rotation *= Time.deltaTime;
+	
+	transform.Translate(left_right,up_down,fore_back);
+	transform.Rotate(0,rotation,0);
+}
